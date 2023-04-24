193f6ca396adbbad054982fcade82f5ed81cf8eb
pedrogatinhos <pedrohenriquebtcc@gmail.com>
2023-04-24 09:44:11 -0300

    match

---
+++ tipos-de-dados/enums/option.rs
@@ -0,0 +1,8 @@
+fn main () {
+    println!("hello world");
+    let some_number = Some(5);
+    // let some_char = Some('e');
+
+    // let absent_number: Option<i32> = None;
+    println!("{:?}", some_number);
+}
