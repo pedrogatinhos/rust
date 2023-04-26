faf7faf69b19cae27b1c89642955ef91cd7ba9ea
pedrogatinhos <pedrohenriquebtcc@gmail.com>
2023-04-26 16:13:19 -0300

    fase1

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,20 +1,39 @@
-use std::simd::u8x32;
-
 use rprompt;
+
 
 fn main() {
    
   soma_numeros();
+  
 
 }
 
 fn soma_numeros() {
-    let mut num_placeholder1 = rprompt::prompt_reply("Digite um numero ");
-    let _num1: u32 = match num_placeholder.trim(){
-        Ok(`Ok`) => println!("ok"),
-        Err(_) =>println!("erro"),
-    }; 
-    let mut _num_placeholder2: String = rprompt::prompt_reply("Digite outro numero ").unwrap();
+    fn test_fun() {
+        println!("numero invalido, reiniciando operação");
+        get_nums(test_fun);
+      };
+      get_nums(test_fun);
+      
+}
+fn get_nums(test_fun: fn()) => u32 {
+    let num1_placeholder = rprompt::prompt_reply("Digite um numero ").unwrap();
+    let num1 = String::from(num1_placeholder.trim());
+    let num1 = match num1.parse::<u32>() {
+        Ok(a) => a,
+        Err(_) =>  return test_fun(),
+    };
 
-    
+    println!("parsed");
+    println!("{num1}");
+
+    let num2_placeholder = rprompt::prompt_reply("Digite um numero ").unwrap();
+    let num2 = String::from(num2_placeholder.trim());
+    let num2: u32 = match num2.parse::<u32>() {
+        Ok(a) => a,
+        Err(_) =>  return test_fun(),
+    };
+    result = num1 + num2;
+    result
+
 }
